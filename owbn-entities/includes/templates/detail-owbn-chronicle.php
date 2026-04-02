3b96d0f256ad51f28d859edafb3131c0b20995bd
Greg Hacke <greghacke@rskgroup.org>
2026-04-01 21:34:56 -0400

    Add id=content to detail templates for fixed-header offset

--- owbn-entities/includes/templates/detail-owbn-chronicle.php
+++ owbn-entities/includes/templates/detail-owbn-chronicle.php
@@ -18,14 +18,14 @@ if ( ! empty( $slug ) && function_exists( 'owc_get_chronicle_detail' ) ) {
     $data = owc_get_chronicle_detail( $slug );
 
     if ( ! is_wp_error( $data ) && ! empty( $data ) && function_exists( 'owc_render_chronicle_detail' ) ) {
-        echo '<div class="owc-template-wrapper">';
+        echo '<div id="content" class="owc-template-wrapper">';
         echo owc_render_chronicle_detail( $data );
         echo '</div>';
     } else {
-        echo '<div class="owc-template-wrapper"><p>' . esc_html__( 'Chronicle not found.', 'owbn-entities' ) . '</p></div>';
+        echo '<div id="content" class="owc-template-wrapper"><p>' . esc_html__( 'Chronicle not found.', 'owbn-entities' ) . '</p></div>';
     }
 } else {
-    echo '<div class="owc-template-wrapper"><p>' . esc_html__( 'No chronicle specified.', 'owbn-entities' ) . '</p></div>';
+    echo '<div id="content" class="owc-template-wrapper"><p>' . esc_html__( 'No chronicle specified.', 'owbn-entities' ) . '</p></div>';
 }
 
 get_footer();
--- owbn-entities/includes/templates/detail-owbn-coordinator.php
+++ owbn-entities/includes/templates/detail-owbn-coordinator.php
@@ -18,14 +18,14 @@ if ( ! empty( $slug ) && function_exists( 'owc_get_coordinator_detail' ) ) {
     $data = owc_get_coordinator_detail( $slug );
 
     if ( ! is_wp_error( $data ) && ! empty( $data ) && function_exists( 'owc_render_coordinator_detail' ) ) {
-        echo '<div class="owc-template-wrapper">';
+        echo '<div id="content" class="owc-template-wrapper">';
         echo owc_render_coordinator_detail( $data );
         echo '</div>';
     } else {
-        echo '<div class="owc-template-wrapper"><p>' . esc_html__( 'Coordinator not found.', 'owbn-entities' ) . '</p></div>';
+        echo '<div id="content" class="owc-template-wrapper"><p>' . esc_html__( 'Coordinator not found.', 'owbn-entities' ) . '</p></div>';
     }
 } else {
-    echo '<div class="owc-template-wrapper"><p>' . esc_html__( 'No coordinator specified.', 'owbn-entities' ) . '</p></div>';
+    echo '<div id="content" class="owc-template-wrapper"><p>' . esc_html__( 'No coordinator specified.', 'owbn-entities' ) . '</p></div>';
 }
 
 get_footer();
